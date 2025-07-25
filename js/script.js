b94907bbea6fe73665944d44dfcc3a75d246ed5d
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-25 15:27:12 +0200

    snack 2

--- js/script.js
+++ js/script.js
@@ -14,3 +14,8 @@
 // funzione somma con arrow function
 const somma = (numOne, numTwo) => numOne + numTwo;
 console.log(somma(7, 10));
+
+// * SNACK 2
+// funzione che restituisce il quadrato di un numero
+const quadrato = (n) => n * n;
+console.log(quadrato(6));
