b7c684ce0572e715617a14c4942737aa3861ea04
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-25 15:25:11 +0200

    somma con arrow function

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@
 // somma(2, 6);
 
 // funzione somma anonima assegnata ad una variabile
-const somma = function (numOne, numTwo) {
-  console.log(numOne + numTwo);
-};
-somma(3, 6);
+// const somma = function (numOne, numTwo) {
+//   console.log(numOne + numTwo);
+// };
+// somma(3, 6);
+
+// funzione somma con arrow function
+const somma = (numOne, numTwo) => numOne + numTwo;
+console.log(somma(7, 10));
