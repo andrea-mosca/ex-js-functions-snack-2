c05c5453c5770c5d1e218c3b48a8d3ccd623b40e
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-25 15:16:03 +0200

    funzione dichiarativa somma

--- js/script.js
+++ js/script.js
@@ -1,32 +1,6 @@
-// **FUNZIONI DENTRO ALTRE FUNZIONI**
-
-function funzioneEsempio(prima, dopo) {
-  prima();
-  setTimeout(() => {
-    console.log("sono passati 1000ms");
-    dopo();
-  }, 1000);
-}
-funzioneEsempio(
-  () => console.log("aspetto"),
-  () => console.log("fine")
-);
-// *GENERATORE DI FUNZIONI
-function createGreeter(greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-}
-const sayHello = createGreeter("hello");
-sayHello("Andrea");
-
-// funzione che genera una funzione che moltiplica un numero
-
-function generateMultiplier(multiplier) {
-  return function (number) {
-    return number * multiplier;
-  };
-}
-
-const double = generateMultiplier(2);
-console.log(double(5));
+// * SNACK 1
+// somma dichiarativa
+const somma = function (numOne, numTwo) {
+  return numOne + numTwo;
+};
+console.log(somma(2, 6));
