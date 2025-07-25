6c30e6820c06b1d11fb274297d7f36dc5b4458fb
andrea-mosca <andrea.moscateli@gmail.com>
2025-07-25 15:23:07 +0200

    funzione assegnata ad una variabile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 // * SNACK 1
 // somma dichiarativa
-function somma(numOne, numTwo) {
+// function somma(numOne, numTwo) {
+//   console.log(numOne + numTwo);
+// }
+// somma(2, 6);
+
+// funzione somma anonima assegnata ad una variabile
+const somma = function (numOne, numTwo) {
   console.log(numOne + numTwo);
-}
-somma(2, 6);
+};
+somma(3, 6);
